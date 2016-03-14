7c690639c7ea35124beb04767d4702c6cf5c5f65
AndreiKh <khadziukou_andrei@bk.ru>
2016-03-14 15:06:34 +0300

    Add samples of different ways to pass parameters to the RESTful web sevice

--- jboss-resteasy/src/main/java/edu/sample/jboss/resteasy/login/LoginService.java
+++ jboss-resteasy/src/main/java/edu/sample/jboss/resteasy/login/LoginService.java
@@ -2,14 +2,23 @@ package edu.sample.jboss.resteasy.login;
 
 import org.jboss.resteasy.annotations.Form;
 
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 /**
- * The sample of using {@link javax.ws.rs.FormParam @FormParam} to bing REST request with data from form. See more
- * details in http://www.mastertheboss.com/jboss-frameworks/resteasy/resteasy-tutorial-part-two-web-parameters
+ * The sample of using {@link FormParam @FormParam} and {@link Form @Form} to bing REST request with data from form.
+ * The samples of using {@link PathParam @PathParam}, {@link QueryParam @QueryParam}, {@link HeaderParam@HeaderParam},
+ * {@link CookieParam @CookieParam} and {@link MatrixParam @MatrixParam}.
+ * See more details in http://www.mastertheboss.com/jboss-frameworks/resteasy/resteasy-tutorial-part-two-web-parameters.
  * Date: 03/10/2016
  *
  * @author Andrei_Khadziukou
@@ -29,6 +38,42 @@ public class LoginService {
     @Produces("text/html")
     public String login(@Form LoginForm form) {
         return "Attempt to sign in by user: " + form.getName() + ", with password: " + form.getPassword() + ".";
+    }
+
+    @GET
+    @Path("login/{authMethod}")
+    @Produces("text/html")
+    public String login(@PathParam("authMethod") String authenticationMethod,
+                        @QueryParam("userName") @DefaultValue("") String userName,
+                        @QueryParam("pass") @DefaultValue("") String password) {
+        if (!"pass".equals(authenticationMethod)) {
+            return "Authentication via '" + authenticationMethod + "' is not supported";
+        }
+
+        if (null == userName || "".equals(userName) || null == password || "".equals(password)) {
+            return "You need to specify user name and password (user name: '" + userName
+                + "', password: '" + password + "').";
+        }
+
+        return "Attempt to sign in by user: '" + userName + "', with password: '" + password + "'.";
+    }
+
+    @GET
+    @Path("loginInfo")
+    @Produces("text/html")
+    public String loginInfo(@HeaderParam("User-Agent") String userAgent, @CookieParam("sessionid") String sessionId) {
+        // http://citforum.ru/internet/html/cookie.shtml
+        // use 'Cookie: sessionid=test' to send the cookie value to RESTful web service
+        return "You use '" + userAgent + "' and your session id is '" + sessionId + "'.";
+    }
+
+    @GET
+    @Path("loginMatrix")
+    @Produces("text/html")
+    public String loginViaMatrixParam(@MatrixParam("authMethod") String authenticationMethod,
+                                      @MatrixParam("userName") String userName,
+                                      @MatrixParam("pass") String password) {
+        return login(authenticationMethod, userName, password);
     }
 
 }
--- jboss-resteasy/src/test/java/edu/sample/jboss/resteasy/login/LoginServiceTest.java
+++ jboss-resteasy/src/test/java/edu/sample/jboss/resteasy/login/LoginServiceTest.java
@@ -26,4 +26,46 @@ public class LoginServiceTest {
         loginForm.setPassword("pass1");
         Assert.assertEquals("Attempt to sign in by user: Andrei, with password: pass1.", loginService.login(loginForm));
     }
+
+    @Test
+    public void testLoginInfo() {
+        Assert.assertEquals("You use 'Mazila Firexof' and your session id is '87483r57837458374'.",
+            loginService.loginInfo("Mazila Firexof", "87483r57837458374"));
+    }
+
+    @Test
+    public void testLoginWithPathAndQueryParams() {
+        Assert.assertEquals("Attempt to sign in by user: 'Andrei', with password: 'pass1'.",
+            loginService.login("pass", "Andrei", "pass1"));
+    }
+
+    @Test
+    public void testLoginWithWrongPathParam() {
+        Assert.assertEquals("Authentication via 'test' is not supported",
+            loginService.loginViaMatrixParam("test", null, null));
+    }
+
+    @Test
+    public void testLoginWithPathParamAndEmptyCredentials() {
+        Assert.assertEquals("You need to specify user name and password (user name: 'Andrei', password: '').",
+                loginService.login("pass", "Andrei", ""));
+    }
+
+    @Test
+    public void testLoginWithMatrixParam() {
+        Assert.assertEquals("Attempt to sign in by user: 'Andrei', with password: 'pass1'.",
+            loginService.loginViaMatrixParam("pass", "Andrei", "pass1"));
+    }
+
+    @Test
+    public void testLoginWithMatrixParamNoAuthenticationMethod() {
+        Assert.assertEquals("Authentication via 'test' is not supported",
+            loginService.loginViaMatrixParam("test", null, null));
+    }
+
+    @Test
+    public void testLoginWithMatrixParamNoPassword() {
+        Assert.assertEquals("You need to specify user name and password (user name: 'Andrei', password: '').",
+            loginService.loginViaMatrixParam("pass", "Andrei", ""));
+    }
 }
