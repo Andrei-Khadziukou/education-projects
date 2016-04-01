adb1c5eaedc55da14c0439abd1f47c04e9b5814d
AndreiKh <khadziukou_andrei@bk.ru>
2016-04-01 20:08:25 +0300

    The lesson 2 (1.2): initialize the spring context and add beans

--- epam/eu-spring-core/src/main/java/com/eu/spring/core/App.java
+++ epam/eu-spring-core/src/main/java/com/eu/spring/core/App.java
@@ -1,23 +1,32 @@
 package com.eu.spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
- * Created by andrei on 31.03.2016.
+ * The application to run samples of using Spring.
+ *
+ * @author Andrei Khadziukou
  */
 public class App {
 
     private Client client;
     private ConsoleEventLogger consoleEventLogger;
 
+    public App(Client client, ConsoleEventLogger consoleEventLogger) {
+        this.client = client;
+        this.consoleEventLogger = consoleEventLogger;
+    }
+
     public void logEvent(String message) {
         consoleEventLogger.logEvent(message.replaceAll(client.getId(), client.getName()));
     }
 
     public static void main(String[] args) {
-        App app = new App();
+        ApplicationContext applicationContext
+            = new ClassPathXmlApplicationContext("com/eu/spring/core/spring-conf.xml");
 
-        app.client = new Client("1", "Eduard");
-        app.consoleEventLogger = new ConsoleEventLogger();
-
+        App app = applicationContext.getBean(App.class);
         app.logEvent("Test message from user 1");
     }
 }
