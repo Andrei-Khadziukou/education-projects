5cd944bde07ebcfcaa4632875eeafc9232fbaee5
AndreiKh <khadziukou_andrei@bk.ru>
2016-05-23 00:15:02 +0300

    Add support of gradle and initiate project structure

---
+++ epam/quartz/src/main/java/edu/sample/quartz/lesson3/SampleRunner.java
@@ -0,0 +1,13 @@
+package edu.sample.quartz.lesson3;
+
+/**
+ * The entry point to run samples.
+ *
+ * @author Andrei Khadziukou
+ */
+public class SampleRunner {
+
+    public static void main(String[] args) {
+
+    }
+}
